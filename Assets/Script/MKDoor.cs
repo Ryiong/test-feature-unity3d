f249d6491d955e0bef7cdca158486dda55755d0b
Ryiong <viethung1209.work@gmail.com>
2024-10-30 20:33:35 +0700

    Relogic code rotate open door

--- Assets/Script/MKDoor.cs
+++ Assets/Script/MKDoor.cs
@@ -8,16 +8,6 @@ public class MKDoor : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip triggerSound;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void TriggerOpen()
     {
